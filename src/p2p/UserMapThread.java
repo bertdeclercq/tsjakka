685c67f5659e81822d88229a851dc16b358b269f
joachim.vanlancker@gmail.com <joachim.vanlancker@gmail.com@52fa36a1-ae95-84ba-e4a1-c7161b97ddae>
2011-10-24 17:47:57 +0000

    vraag in usermapthread, check it out.

--- src/p2p/UserMapThread.java
+++ src/p2p/UserMapThread.java
@@ -18,28 +18,32 @@ import java.util.logging.Logger;
 public class UserMapThread implements Runnable {
 
     private Map<InetAddress, String> userMap;
+    /*
+     * met static setter map opnieuw opvullen in p2pclient of gewoon met 
+     * P2Pclient.getInstance().getUsermap().remove werken?
+     */
 
     @Override
     public void run() {
         userMap = P2Pclient.getInstance().getUserMap();
-        do{
-        for (InetAddress ip : userMap.keySet()) {
-            try {
-                if (!ip.isReachable(2000)) {
-                    userMap.remove(ip);
+        do {
+            for (InetAddress ip : userMap.keySet()) {
+                try {
+                    if (!ip.isReachable(2000)) {
+                        userMap.remove(ip);
+                    }
+
+                } catch (IOException ex) {
+                    Logger.getLogger(UserMapThread.class.getName()).log(Level.SEVERE, null, ex);
                 }
-                 
-            } catch (IOException ex) {
+
+            }
+            P2Pclient.getInstance().printUserMap();
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException ex) {
                 Logger.getLogger(UserMapThread.class.getName()).log(Level.SEVERE, null, ex);
             }
-            
-        }
-        P2Pclient.getInstance().printUserMap();
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException ex) {
-            Logger.getLogger(UserMapThread.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        }while(true);
+        } while (true);
     }
 }
