20013418da7f8a71fcb884e8de5529affc906ecd
xunobulax@gmail.com <xunobulax@gmail.com@52fa36a1-ae95-84ba-e4a1-c7161b97ddae>
2011-10-25 23:47:43 +0000

    Zoekt nu in submappen en negeert verborgen mappen.

--- trunk/src/p2p/SharedFiles.java
+++ trunk/src/p2p/SharedFiles.java
@@ -1,6 +1,7 @@
 package p2p;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,17 +11,30 @@ public class SharedFiles {
     private static List<String> sharedList = new ArrayList<String>();
 
     public static void main(String[] args) {
-        File dir = new File(SHARED_DIR);
-        File[] children = dir.listFiles();
+        findShared(SHARED_DIR);
+        System.out.println(sharedList.toString());
+    }
+
+    public static void findShared(String strDir) {
+        File dir = new File(strDir);
+        FilenameFilter filter = new FilenameFilter() {
+
+            @Override
+            public boolean accept(File dir, String name) {
+                return !name.startsWith(".");
+            }
+        };
+        File[] children = dir.listFiles(filter);
         if (children == null) {
             System.out.println("foute map");
         } else {
             for (int i = 0; i < children.length; i++) {
                 if (!children[i].isDirectory()) {
                     sharedList.add(children[i].getName());
+                } else {
+                    findShared(children[i].getPath());
                 }
             }
         }
-        System.out.println(sharedList.toString());
     }
 }
