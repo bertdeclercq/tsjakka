2649f0b798764da34913cab514bf5e6c66271566
cee@live.be <cee@live.be@52fa36a1-ae95-84ba-e4a1-c7161b97ddae>
2011-11-01 13:15:40 +0000

    Geprobeerd een eerste versieke van mijne Message erop te krijgen.

---
+++ trunk/src/p2p/Message.java
@@ -0,0 +1,75 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p2p;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Gebruiker
+ */
+public class Message implements Serializable {
+    
+    private String tag;
+    private boolean onlineMessage = false, onlineTooMessage = false, signOutMessage = false;
+    
+    private static String ONLINE = "<online>",
+                          ONLINETOO = "<online2>",
+                          SIGNOUT = "<signout>";
+    
+    
+    public Message(String tag)
+    {
+        this.tag = tag;
+    }
+            
+    public void checkStatusTag()
+    {
+        if(tag.equalsIgnoreCase(ONLINE))
+            setOnlineMessage(true);
+        if(tag.equalsIgnoreCase(ONLINETOO))
+            setOnlineTooMessage(true);
+        if(tag.equalsIgnoreCase(SIGNOUT))
+            setSignOutMessage(true);
+    }
+
+    public boolean isOnlineMessage() {
+        return onlineMessage;
+    }
+
+    public void setOnlineMessage(boolean onlineMessage) {
+        this.onlineMessage = onlineMessage;
+    }
+
+    public boolean isOnlineTooMessage() {
+        return onlineTooMessage;
+    }
+
+    public void setOnlineTooMessage(boolean onlineTooMessage) {
+        this.onlineTooMessage = onlineTooMessage;
+    }
+
+    public boolean isSignOutMessage() {
+        return signOutMessage;
+    }
+
+    public void setSignOutMessage(boolean signOutMessage) {
+        this.signOutMessage = signOutMessage;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        if (tag.startsWith("<") && tag.endsWith(">"))
+            this.tag = tag;
+        else
+            throw new IllegalArgumentException("Tag is not in the right format!");
+    }
+    
+    
+    
+}
