bcbc3270720d9a5ec8fcfc64dd2fa86b326d8d3e
xunobulax@gmail.com <xunobulax@gmail.com@52fa36a1-ae95-84ba-e4a1-c7161b97ddae>
2011-11-01 18:53:10 +0000

    Laatste versie van SharedFiles. (Multithreaded safe)

--- trunk/src/p2p/SharedFiles.java
+++ trunk/src/p2p/SharedFiles.java
@@ -1,21 +1,51 @@
 package p2p;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FilenameFilter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class SharedFiles {
 
-    private static String SHARED_DIR = "./shared";
-    private static List<String> sharedList = new ArrayList<String>();
+    private final static String CONFIG_FILE = "config";
+    private static SharedFiles instance;
+    private static List<String> sharedList;
+    private static Properties properties;
 
-    public static void main(String[] args) {
-        findShared(SHARED_DIR);
-        System.out.println(sharedList.toString());
+    private SharedFiles() {        
+        sharedList = new ArrayList<String>();
+        properties = new Properties();
+        updateSharedList();
+    }
+    
+    public static synchronized SharedFiles getInstance() {
+        if (instance == null)
+            instance = new SharedFiles();
+        return instance;
     }
 
-    public static void findShared(String strDir) {
+    public List<String> getSharedList() {
+        return sharedList;
+    }
+    
+    public static void updateSharedList() {
+        try {
+            FileInputStream in = new FileInputStream(CONFIG_FILE);
+            properties.load(in);
+            in.close();
+        } catch (IOException ex) {
+            Logger.getLogger(SharedFiles.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        findShared(properties.getProperty("directory"));
+    }
+
+    private static void findShared(String strDir) {
         File dir = new File(strDir);
         FilenameFilter filter = new FilenameFilter() {
 
@@ -36,5 +66,17 @@ public class SharedFiles {
                 }
             }
         }
+    }
+    
+    public void changeDirectory(String dir) {
+        properties.setProperty("directory", dir);
+        try {
+            FileOutputStream out = new FileOutputStream(CONFIG_FILE);
+            properties.store(out, null);
+            out.close();
+        } catch (IOException ex) {
+            Logger.getLogger(SharedFiles.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        updateSharedList();
     }
 }
