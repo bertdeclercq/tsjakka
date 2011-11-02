53641405b6db6fee5b0ab2cb5bd799da448b6aa3
joachim.vanlancker@gmail.com <joachim.vanlancker@gmail.com@52fa36a1-ae95-84ba-e4a1-c7161b97ddae>
2011-11-02 13:53:35 +0000

    stukje van de filetransferererererererrereererrer gemaakt

--- trunk/src/p2p/DownloadRequestListener.java
+++ trunk/src/p2p/DownloadRequestListener.java
@@ -8,7 +8,11 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.SocketException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,9 +22,11 @@ import java.util.logging.Logger;
  */
 public class DownloadRequestListener implements Runnable {
 
-    private static final int PORT = 1237;
+    private static ServerSocket servSocket;
+    private static final int TCPPORT = 1238;
+    private static final int UDPPORT = 1237;
     private static DatagramSocket dgramSocket;
-    private static DatagramPacket inPacket, outPacket;
+    private static DatagramPacket inPacket;
     private static byte[] buffer;
     private static String messageIn;
     private static InetAddress clientAddress;
@@ -32,7 +38,10 @@ public class DownloadRequestListener implements Runnable {
     @Override
     public void run() {
         try {
-            dgramSocket = new DatagramSocket(PORT);
+            dgramSocket = new DatagramSocket(UDPPORT);
+            Socket link = null;
+            ExecutorService threadExecutor = Executors.newCachedThreadPool();
+            String filename;
             do {
                 buffer = new byte[256];
                 inPacket = new DatagramPacket(buffer, buffer.length);
@@ -40,12 +49,15 @@ public class DownloadRequestListener implements Runnable {
                 clientAddress = inPacket.getAddress();
                 messageIn = new String(inPacket.getData(), 0, inPacket.getLength());
                 System.out.println(messageIn);
+                servSocket = new ServerSocket(TCPPORT);
+                link = servSocket.accept();
+                filename = messageIn;
+                threadExecutor.execute(new FileTransferer(link,filename));
             } while (true);
         } catch (IOException ex) {
             Logger.getLogger(DownloadRequestListener.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        finally{
+        } finally {
             dgramSocket.close();
         }
-    } 
+    }
 }
