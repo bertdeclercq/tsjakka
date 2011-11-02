0a532670791301b524a867c44deb84b1292ddc38
xunobulax@gmail.com <xunobulax@gmail.com@52fa36a1-ae95-84ba-e4a1-c7161b97ddae>
2011-11-02 10:26:08 +0000

    Proberen voor lijst door te sturen.

--- trunk/src/p2p/BroadcastThread.java
+++ trunk/src/p2p/BroadcastThread.java
@@ -5,11 +5,15 @@
 package p2p;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -22,7 +26,7 @@ public class BroadcastThread implements Runnable {
     private MulticastSocket multiSocket;
     private InetAddress multiGroup;
     private InetAddress ownIp;
-    private DatagramPacket multiPacket;
+    private DatagramPacket multiPacket, sharedListPacket;
 
     /* Elke 10 sec wordt een multicast packet gestuurd om te kijken
      * wie er op de LAN zit
@@ -44,6 +48,14 @@ public class BroadcastThread implements Runnable {
                 buf = message.getBytes();
                 multiPacket = new DatagramPacket(buf, buf.length, multiGroup, 4446);
                 multiSocket.send(multiPacket);
+                //vestuur lijst met gedeelde bestanden
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                ObjectOutputStream out = new ObjectOutputStream(stream);
+                out.writeObject(SharedFiles.getInstance().getSharedList());
+                out.close();
+                buf = stream.toByteArray();
+                sharedListPacket = new DatagramPacket(buf, buf.length, multiGroup, 4446);
+                multiSocket.send(sharedListPacket);
                 System.out.println("sent");
                 Thread.sleep(8000);
             } while (true);
