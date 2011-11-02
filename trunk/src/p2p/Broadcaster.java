27eff51ec5696aff088fc2cf1c00219577806001
cee@live.be <cee@live.be@52fa36a1-ae95-84ba-e4a1-c7161b97ddae>
2011-11-02 13:09:22 +0000

    Broadcaster aangepast aan nieuw protocol.

--- trunk/src/p2p/Broadcaster.java
+++ trunk/src/p2p/Broadcaster.java
@@ -7,6 +7,7 @@ package p2p;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
@@ -29,21 +30,17 @@ public class Broadcaster implements Runnable {
      */
     @Override
     public void run() {
-        BufferedReader userEntry = new BufferedReader(new InputStreamReader(System.in));
         try {
             multiSocket = new MulticastSocket(4446);
             multiGroup = InetAddress.getByName("230.0.0.1");
             ownIp = InetAddress.getLocalHost();
-            String OwnAddress = ownIp.getHostAddress().toString();
-            String ownHostname = ownIp.getHostName();
             Message message;
             do {
-                String userMessage = userEntry.readLine();
                 multiSocket.joinGroup(multiGroup);
                 byte[] buf = new byte[256];
-                String content = OwnAddress + "*" + ownHostname + "*" + userMessage;
+                //String content = OwnAddress + "*" + ownHostname;
                 //buf = message.getBytes();
-                message = new Message("<online>", SharedFiles.getInstance().getSharedList());
+                message = new Message("<online>", ownIp.getHostAddress().toString(), ownIp.getHostName(), SharedFiles.getInstance().getSharedList());
                 multiPacket = new DatagramPacket(buf, buf.length, multiGroup, 4446);
                 multiSocket.send(multiPacket);
                 System.out.println("sent");
