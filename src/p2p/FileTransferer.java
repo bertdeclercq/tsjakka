2020f9d937c5eb99a2ad6b44a02035e65e282c41
joachim.vanlancker@gmail.com <joachim.vanlancker@gmail.com@52fa36a1-ae95-84ba-e4a1-c7161b97ddae>
2011-11-03 10:19:47 +0000

    tcp server gedeelte gemaakt ( filetransferlistner ). die staat constant te luisteren voor tcp downloadrequest op te vangen en als wanneer die een ontvangt wordt een nieuwe thread gestart waar de request wordt afgehandeld.

--- src/p2p/FileTransferer.java
+++ src/p2p/FileTransferer.java
@@ -5,9 +5,11 @@
 package p2p;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 
@@ -20,15 +22,18 @@ public class FileTransferer implements Runnable {
     private Socket link;
     private String filename;
 
-    public FileTransferer(Socket link,String filename) {
+    public FileTransferer(Socket link) {
         this.link = link;
-        this.filename = filename;
     }
 
     @Override
     public void run() {
+        String inMessage;
         try {
-            File myFile = new File("config");
+            BufferedReader in = new BufferedReader( new InputStreamReader(link.getInputStream()));
+            inMessage  = in.readLine();
+            filename = inMessage;
+            File myFile = new File(filename);
             byte[] mybytearray = new byte[(int) myFile.length()];
             FileInputStream fis = new FileInputStream(myFile);
             BufferedInputStream bis = new BufferedInputStream(fis);
