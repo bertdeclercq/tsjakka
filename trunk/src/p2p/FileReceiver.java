cded10501ed2bcab5ee08bbf20286cf7fe60cf2b
joachim.vanlancker@gmail.com <joachim.vanlancker@gmail.com@52fa36a1-ae95-84ba-e4a1-c7161b97ddae>
2011-11-03 13:01:23 +0000

    tis van da (filereceiver deel van downloadrequest gemaakt)

---
+++ trunk/src/p2p/FileReceiver.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p2p;
+
+/**
+ *
+ * @author Joachim
+ */
+public class FileReceiver implements Runnable{
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    
+}
