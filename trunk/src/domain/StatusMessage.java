806e1119983b58e8c1384ba81a13d8e70cf182e0
jimmyhuybrecht@gmail.com <jimmyhuybrecht@gmail.com@52fa36a1-ae95-84ba-e4a1-c7161b97ddae>
2011-12-08 10:09:40 +0000

    javadoc vo statusmessage

--- trunk/src/domain/StatusMessage.java
+++ trunk/src/domain/StatusMessage.java
@@ -9,18 +9,29 @@ import java.util.Observer;
 
 /**
  *
- * @author Jimmy
+ * This class is responsible for holding the status
+ * that will be sent to the statusarea in the gui screen
  */
 public class StatusMessage extends Observable {
 
     private String status;
     
-    
+    /**
+     * 
+     * @param stat 
+     * initialize a newly created statusmessage object with stat
+     * set as status.
+     */
     public StatusMessage(String stat){
         status = stat;
         statusChanged();
     }
 
+    
+    /**
+     * 
+     * @return status The status that is currently in the statusmessage class
+     */
     public String getStatus() {
         return status;
     }
@@ -28,11 +39,21 @@ public class StatusMessage extends Observable {
     
     
    
-
+/**
+     * 
+     * @param newStatus The status that has to be set so the
+     * observer update method will be triggered
+     */
     public void addStatus(String newStatus) {
         status = newStatus;
         statusChanged();
     }
+    /**
+     * 
+     * @param observer 
+     * Override of the standard addObserver method
+     * 
+     */
     @Override
     public void addObserver(Observer observer)
     {
@@ -40,6 +61,10 @@ public class StatusMessage extends Observable {
         observer.update(this, getStatus());
     }
     
+    /**
+     * This is the method which lets the observers know that the
+     * subject has canged and that it has to update
+     */
     public void statusChanged(){
         setChanged();
         notifyObservers(status);
