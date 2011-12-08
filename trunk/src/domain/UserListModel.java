478fd787541b200d862493389bdcc0a421151c0d
cee@live.be <cee@live.be@52fa36a1-ae95-84ba-e4a1-c7161b97ddae>
2011-12-08 11:30:03 +0000

    Javadoc gemaakt en wat nutteloze dingen verwijderd in domeincontroller.

--- trunk/src/domain/UserListModel.java
+++ trunk/src/domain/UserListModel.java
@@ -10,28 +10,43 @@ import javax.swing.AbstractListModel;
 
 /**
  *
- * @author Gebruiker
+ * Provides the capability to create a listmodel and fill the gui with data from DomeinController.
  */
 public class UserListModel extends AbstractListModel implements Observer {
     
     private DomeinController dc;
 
+    /**
+     * Initializes a newly created Filter object.
+     * @param dc The current used domeincontroller in the program
+     */
     public UserListModel(DomeinController dc) {
         this.dc = dc;
         dc.addObserver(this);
     }
 
+    /**
+     * Returns the size of the usermap
+     * @return the size of the usermap.
+     */
     @Override
     public int getSize() {
         return dc.getUserMapSize();
     }
 
+    /**
+     * Returns the specified element at the given index.
+     * @param index The index of the list
+     * @return the username of the user.
+     */
     @Override
     public Object getElementAt(int index) {
        return dc.getUserNameUser(index);
         
     }
-
+    /**
+     * Updates the userlist when a users leaves or enters the program. 
+     */
     @Override
     public void update(Observable o, Object arg) {
        this.fireContentsChanged(this, 0, dc.getUserMapSize());
